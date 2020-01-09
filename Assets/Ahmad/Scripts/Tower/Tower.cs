d6dc5c3883e7b6b1de1e417c3bf65e058d5d3333
Ahmad Sbeiti <ahmadsbeiti@outlook.com>
2020-01-09 12:19:30 -0500

    Creation of the generic tower class
    Pole-61
    Reviewer: Marc-Antoine
    Name: Generic Tower
    
     - All attributes needed for a tower
     - Getters/Setters

---
+++ Assets/Ahmad/Scripts/Tower/Tower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Tower {
+    public Vector3 Position { get; protected set; }
+    public GameObject TpButtonPos { get; protected set; }
+    public TowerType Type { get; protected set; }
+    public bool IsPlayerActive { get; protected set; }
+    public float Range { get; protected set; }
+    public float Damage { get; protected set; }
+    public float DefaultAttackCooldown { get; protected set; }
+
+    public abstract void PreInitialize();
+    public abstract void Initialize();
+    public abstract void Refresh();
+    public abstract void PhysicsRefresh();
+}
