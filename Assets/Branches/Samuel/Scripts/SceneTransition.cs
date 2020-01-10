eb85c11495552355c5a25dd709236a5a48c49e71
gosfi <44752770+gosfi@users.noreply.github.com>
2020-01-10 11:11:43 -0500

    scene transition plus cheat
    POLE-85 Transition: unity scene transition
    
    reviewer: JP
    
    desc: added the scene transition, added a simple dev test cheat inputfield

---
+++ Assets/Branches/Samuel/Scripts/SceneTransition.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string nameSceneAction;
+    public string nameBarScene;
+    public GameObject canvas;
+    public TMP_InputField cheats;
+
+    void Start()
+    {
+        canvas.SetActive(false);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.BackQuote))
+        {
+            canvas.SetActive(true);
+        }
+
+    }
+    private void loadActionScene()
+    {
+        SceneManager.LoadScene(nameSceneAction);
+    }
+
+    public void checkCheat()
+    {
+        if (cheats.text.Equals("action"))
+        {
+            loadActionScene();
+        }
+        if (cheats.text.Equals("bar"))
+        {
+            loadBarScene();
+        }
+    }
+
+    private void loadBarScene()
+    {
+        SceneManager.LoadScene(nameBarScene);
+    }
+}
