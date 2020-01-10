cf80618c47c0af0f5ca32b213ffabacdc01f61c7
astonge <astonge@isi-mtl.com>
2020-01-09 20:31:11 -0500

    Managers Scripts Created
    POLE-91
    NO REVIEWER
    Managers scripts created in main Script Folder. TowerManager Needs to be moved to the appropriated folder and changed for Flow architecture.

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    static public Main instance { get; private set; }
+
+    private Game game;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        game = Game.Instance;
+        game.PreInitialize();
+
+    }
+
+    private void Start()
+    {
+        game.Initialize();
+    }
+
+    private void Update()
+    {
+        game.Refresh();
+    }
+
+    private void FixedUpdate()
+    {
+        game.PhysicsRefresh();
+    }
+
+}
---
+++ Assets/Scripts/TrapManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapManager : Flow
+{
+    #region Singleton
+    static private TrapManager instance = null;
+
+    static public TrapManager Instance
+    {
+        get {
+            return instance ?? (instance = new TrapManager());
+        }
+    }
+
+    #endregion
+
+    override public void PreInitialize()
+    {
+
+    }
+
+    override public void Initialize()
+    {
+
+    }
+
+    override public void Refresh()
+    {
+
+    }
+
+    override public void PhysicsRefresh()
+    {
+
+    }
+}
