92b64953bb743357584ce5c88e6d2e453c0f7dab
Ahmad Sbeiti <ahmadsbeiti@outlook.com>
2020-01-10 14:56:30 -0500

    bomb feeder class finished & small fixes
    Pole-20
    Reviewer: Marc-Antoine
     - Instantiate of bomb feeder through script
     - Instantiate of bombs through script
     - Tube system for the bombs to get up
     - Added a test manager for my towers

---
+++ Assets/Branches/Ahmad/Scripts/Bomb/Bomb.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb {
+    public Vector3 Position;
+
+    public Bomb() {
+        Position = new Vector3(0, 0, 0);
+    }
+
+    public Bomb(Vector3 position) {
+        Position = position;
+    }
+}
