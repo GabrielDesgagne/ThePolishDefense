f15d0869593c175e8a1c618a8390e9677f78ce05
Ahmad Sbeiti <ahmadsbeiti@outlook.com>
2020-01-15 11:46:36 -0500

    Cleaned up tower code & assets
    Pole-182
    Reviewer: None
     - Modified code to follow the conventions
     - Moved all assets relating to tower into the main assets folders
     - Added function to create Tower

---
+++ Assets/Scripts/Projectile/Bomb.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Bomb : Projectile {
+    public Bomb(GameObject ob)
+    {
+        Obj = ob;
+        SlerpPct = 0;
+        Type = ProjectileType.BOMB;
+    }
+}
---
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile {
+    public GameObject Obj { get; set; }
+    public ProjectileType Type { get; set; }
+    public Vector3 StartPos { get; set; }
+    public Vector3 TargetPos { get; set; }
+    public float SlerpPct { get; set; }
+
+    public void MoveToTarget()
+    {
+        if (SlerpPct < 1)
+        {
+            Obj.transform.position = Vector3.Slerp(StartPos, TargetPos, SlerpPct);
+            SlerpPct += 0.01f;
+        }
+    }
+
+    public void Reset()
+    {
+        Obj.SetActive(false);
+        Obj.transform.position = StartPos;
+        SlerpPct = 0;
+    }
+}
