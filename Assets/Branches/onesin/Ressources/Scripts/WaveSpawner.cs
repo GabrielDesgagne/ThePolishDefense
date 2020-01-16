3b7db6a873f60690d63ca668e4be986f68e19d9c
onesin-19 <52038908+onesin-19@users.noreply.github.com>
2020-01-16 10:56:49 -0500

    Wave: ending path
    POLE-82
    reviewer: none
    animation door

--- Assets/Branches/onesin/Ressources/Scripts/WaveSpawner.cs
+++ Assets/Branches/onesin/Ressources/Scripts/WaveSpawner.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class WaveSpawner : MonoBehaviour {
 
     public static int EnemiesAlive = 0;
+    public Logic gameLogic;
 
-    //[SerializeField]
     public Wave[] waves;
 
     [SerializeField]
-    private Transform spawnPoint;
+    public Transform spawnPoint;
 
     [SerializeField]
     private float timeBetweenWaves = 5f;
@@ -21,9 +22,10 @@ public class WaveSpawner : MonoBehaviour {
     [SerializeField]
     private GameObject countdown;
     private Countdown waveCountdownTimer;
-
+    //just for test
+    Dictionary<EnemyType, GameObject> enemyPrefab;
     private void Start() { Initialize(); }
-    private void Update() { Refresh(); }
+    private void Update() { Refresh(enemyPrefab); }
    
     public void Initialize()
     {
@@ -37,16 +39,22 @@ public class WaveSpawner : MonoBehaviour {
 
     }
 
-    public void Refresh() {
+    public void Refresh(Dictionary<EnemyType, GameObject> enemyPrefab) {
         //condition to restart countdown
         if (EnemiesAlive > 0)
         {
             return;
         }
 
+        if (waveIndex == waves.Length)
+        {
+            gameLogic.WinLevel();
+            this.enabled = false;
+        }
+
         if (waveCountdownTimer.countdown <= 0f)
         {
-            StartCoroutine(SpawnWave());
+            StartCoroutine(SpawnWave(enemyPrefab));
             waveCountdownTimer.countdown = timeBetweenWaves;
             return;
         }
@@ -55,16 +63,29 @@ public class WaveSpawner : MonoBehaviour {
 
     }
 
-    IEnumerator SpawnWave()
+    IEnumerator SpawnWave(Dictionary<EnemyType, GameObject> enemyPrefab)
     {
-        
+        PlayerStats.nextLevel();
+
+
         Wave wave = waves[waveIndex];
 
         EnemiesAlive = wave.enemy.Count;
-
+        /*for (int i = 0; i < wave.types.Length; i++)
+        {
+            for (int j = 0; j < wave.types[i].number; j++)
+            {
+                GameObject newEnemy = SpawnEnemy(enemyPrefab[wave.types[i].type]);
+                Enemy e = newEnemy.GetComponent<Enemy>();   //get the enemy component on the newly created obj
+                EnemyManager.Instance.toAdd.Push(e);
+                yield return new WaitForSeconds(1f / wave.rate);
+            }
+        }*/
         for (int i = 0; i < wave.enemy.Count; i++)
         {
-            SpawnEnemy(wave.enemy[i]);
+            GameObject newEnemy = SpawnEnemy(wave.enemy[i]);
+            Enemy e = newEnemy.GetComponent<Enemy>();   //get the enemy component on the newly created obj
+            //EnemyManager.Instance.toAdd.Push(e);
             yield return new WaitForSeconds(1f / wave.rate);
         }
 
@@ -72,9 +93,9 @@ public class WaveSpawner : MonoBehaviour {
 
     }
 
-    void SpawnEnemy(GameObject enemy)
+    GameObject SpawnEnemy(GameObject enemy)
     {
-        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+        return Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
     }
 }
 
