faad34d2ab75fde9b3d3e096bf93b3846396184e
onesin-19 <52038908+onesin-19@users.noreply.github.com>
2020-01-17 15:52:56 -0500

    Animation enemy
    new enemy type with animation

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EnemyType { FAST, SIMPLE, SLOW }
+public enum EnemyType { FAST, SIMPLE, SLOW,KNIGHT }
 public class EnemyManager : Flow
 {
     #region Singleton
