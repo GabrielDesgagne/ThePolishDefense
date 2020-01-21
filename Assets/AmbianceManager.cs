a513d1309ae63fad4016551dd9f21390ac77ea14
gosfi <44752770+gosfi@users.noreply.github.com>
2020-01-21 11:57:48 -0500

    added enum for sounds type
    ambiance manager
    
    rev: jp

--- Assets/AmbianceManager.cs
+++ Assets/AmbianceManager.cs
@@ -6,27 +6,41 @@ public class AmbianceManager : Flow
 {
     private static AmbianceManager instance = null;
     public static AmbianceManager Instance { get { return instance ?? (instance = new AmbianceManager()); } }
+    [Header("Room audioClip")]
     AudioClip fireSound;
     AudioClip musicRoom;
+    AudioClip mapMusic;
 
-    GameObject fireplace;
-    GameObject bgMusicRoom;
+    [Header("Room audioSource")]
+    GameObject soundEffects;
+    GameObject bgMusic;
+
 
 
     public override void PreInitialize()
     {
-        fireplace = new GameObject();
-        fireplace.AddComponent<AudioSource>();
+        // prepares sounds for the room
+        soundEffects = new GameObject();
+        bgMusic = new GameObject();
+        bgMusic.name = "bgMusic";
+        soundEffects.name = "soundEffects";
+        soundEffects.AddComponent<AudioSource>();
+        bgMusic.AddComponent<AudioSource>();
         fireSound = Resources.Load<AudioClip>("SFX/RoomSound/Fireplace1");
-        bgMusicRoom = new GameObject();
-        bgMusicRoom.AddComponent<AudioSource>();
-        
+
+        musicRoom = Resources.Load<AudioClip>("SFX/Soundtrack/Wild_Boars_Inn");
+
+        //prepares sounds for the map
+        mapMusic = Resources.Load<AudioClip>("SFX/Soundtrack/fight");
+
+
         base.PreInitialize();
     }
     public override void Initialize()
     {
         base.Initialize();
-        playSoundsRoom();
+         playMapMusic();
+        //playSoundsRoom();
     }
     public override void Refresh()
     {
@@ -43,10 +57,19 @@ public class AmbianceManager : Flow
 
     public void playSoundsRoom()
     {
-        fireplace.GetComponent<AudioSource>().clip = fireSound;
-        fireplace.GetComponent<AudioSource>().Play();
-        // music.Play();
-        Debug.Log("hello");
+        soundEffects.GetComponent<AudioSource>().clip = fireSound;
+        soundEffects.GetComponent<AudioSource>().volume = 0.04f;
+        soundEffects.GetComponent<AudioSource>().Play();
+        bgMusic.GetComponent<AudioSource>().clip = musicRoom;
+        bgMusic.GetComponent<AudioSource>().volume = 0.04f;
+        bgMusic.GetComponent<AudioSource>().Play();
+    }
+
+    public void playMapMusic()
+    {
+        bgMusic.GetComponent<AudioSource>().clip = mapMusic;
+        bgMusic.GetComponent<AudioSource>().volume = 0.04f;
+        bgMusic.GetComponent<AudioSource>().Play();
     }
 
 }
