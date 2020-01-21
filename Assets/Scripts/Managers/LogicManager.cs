d2a6aed0314b7f82d267f473493a4c9e7fffe074
onesin-19 <52038908+onesin-19@users.noreply.github.com>
2020-01-21 14:28:25 -0500

    update logic game
    POLE none
    reviewer: none
    desc: update of the logic game

--- Assets/Scripts/Managers/LogicManager.cs
+++ Assets/Scripts/Managers/LogicManager.cs
@@ -9,13 +9,16 @@ public class LogicManager : Flow
 
     static public LogicManager Instance
     {
-        get {
+        get
+        {
             return instance ?? (instance = new LogicManager());
         }
     }
 
     #endregion
 
+    LevelSystem levelSystem;
+    public static bool gameIsOver;
     override public void PreInitialize()
     {
 
@@ -23,17 +26,50 @@ public class LogicManager : Flow
 
     override public void Initialize()
     {
+       /* float hp;
+        int nbEnemies = 0;
+        for (int k = 0; k < levelSystem.levels[(int)levelSystem.currentLevel].waves.Length; k++)
+        {
+            for (int i = 0; i < levelSystem.levels[(int)levelSystem.currentLevel].waves[k].types.Length; i++)
+            {
+                nbEnemies += levelSystem.levels[(int)levelSystem.currentLevel].waves[k].types[i].number;
 
+            }
+        }
+
+        hp = (float)(0.25 * nbEnemies);
+        PlayerStats.Hp = hp;
+        gameIsOver = false;*/
     }
 
     override public void Refresh()
     {
+        /*if (gameIsOver)
+        {
+            return;
+        }
 
+        if (PlayerStats.Hp <= 0)
+        {
+            EndGame();
+        }*/
     }
 
     override public void PhysicsRefresh()
     {
 
+    }
+
+    void EndGame()
+    {
+        gameIsOver = true;
+        //go to menu if exist
+    }
+
+    public void WinLevel()
+    {
+        gameIsOver = true;
+        //go to next level after transition
     }
 
     override public void EndFlow()
